c4ea714b9771e51869ec4a702e7f4faa3f2a6473
Pietro Rhyan <pietroqjg@gmail.com>
2026-03-18 18:42:30 -0300

    feat: add projects carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-export default function Home() {
+import { Carousel } from "@/components/carousel";
 
+export default function Home() {
   return (
-    <main className="h-[calc(100dvh-69px)] max-w-216 mx-auto border-x border-dashed border-x-dark-gray px-9">
-      Olá
+    <main className="relative h-[calc(100dvh-69px)] max-w-216 mx-auto border-x border-dashed border-x-dark-gray">
+      <Carousel />
     </main>
-  )
+  );
 }
