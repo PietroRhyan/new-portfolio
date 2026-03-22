7b0fbe5e870cfe40529aa9cf30cda7f195a3b0d1
Pietro Rhyan <pietroqjg@gmail.com>
2026-03-22 15:12:39 -0300

    feat: add card components and experiences

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
 import { useTranslations } from "next-intl";
 import { Intro } from "@/components/intro";
+import { Card, type CardProps } from "@/components/ui/card";
 import { Section } from "@/components/ui/section";
+
+const mainExperiences: CardProps[] = [
+  {
+    title: "Ativos Capital",
+    role: "Desenvolvedor Frontend",
+    startDate: "Nov 2024",
+    endDate: "Presente",
+    description:
+      "Atuei em duas empresas do grupo Ativos Capital, desenvolvi e dei manuntanção nas principais páginas da empresa EasyCDP, lideirei features core da plataforma, mantive um grande Design System e mais.",
+    href: "https://www.rhyann.com/",
+    img: "/images/me.png",
+  },
+];
+
+const experiences: CardProps[] = [
+  {
+    title: 'CMA Digital Music',
+    role: 'Freelancer',
+    description: 'Desenvolvimento de ponta-a-ponta (design ao deploy) de uma landing page para um cliente do setor musical',
+    href: 'https://www.cmadigitalmusic.com/',
+  },
+  {
+    title: 'Fros Venture',
+    role: 'Desenvolvedor Fullstack',
+    description: 'Side-hustle criado com amigos da faculdade para promover projetos open-source de devs indies',
+    href: 'https://www.fros.dev/',
+  },
+]
 
 export default function Home() {
   const t = useTranslations("home.navbar");
@@ -14,7 +43,34 @@ export default function Home() {
         title={t("experiences")}
         description="Experiência profissional, projetos e outros..."
       >
-        Oi porra
+        <div className="w-full flex flex-col gap-6" >
+          {mainExperiences.map((exp) => (
+            <Card
+              key={exp.title}
+              title={exp.title}
+              startDate={exp.startDate}
+              endDate={exp.endDate}
+              role={exp.role}
+              description={exp.description}
+              href={exp.href}
+              img={exp.img}
+            />
+          ))}
+
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6" >
+            {experiences.map((exp) => (
+              <Card
+                key={exp.title}
+                title={exp.title}
+                role={exp.role}
+                description={exp.description}
+                href={exp.href}
+                style="compact"
+              />
+            ))}
+          </div>
+        </div>
+
       </Section>
     </main>
   );
---
+++ src/components/ui/card/index.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+
+export interface CardProps {
+  img?: string;
+  title: string;
+  role: string;
+  description: string;
+  href: string;
+  startDate?: string;
+  endDate?: string;
+  style?: "full" | "compact";
+}
+
+export function Card({
+  img,
+  title,
+  role,
+  description,
+  href,
+  startDate,
+  endDate,
+  style = "full",
+}: CardProps) {
+  const t = useTranslations("commom");
+
+  const formattedDate = `${startDate} - ${endDate}`;
+
+  return (
+    <Link href={href} className="rounded-2xl group">
+      <div className="w-full bg-background rounded-2xl border border-dark-gray cursor-pointer flex flex-col sm:flex-row overflow-hidden shadow-container hover:bg-radial-[at_10%_100%] transition-colors duration-200 hover:border-gray/20 hover:from-[#2F2F2F] hover:to-background hover:to-50%">
+        {style === "full" && img && (
+          <div className="relative w-full h-52 sm:w-[166px] sm:h-auto shrink-0">
+            <Image
+              src={img}
+              alt="Card Image"
+              fill
+              quality={80}
+              draggable={false}
+              className="object-cover"
+            />
+          </div>
+        )}
+
+        <div className="w-full h-full p-6 flex flex-col gap-4">
+          <div className="w-full flex items-start justify-between gap-6">
+            <div className="flex flex-col gap-0.5">
+              <h4 className="text-lg">{title}</h4>
+              {style === "full" && startDate && (
+                <span className="text-xs text-gray/50">{formattedDate}</span>
+              )}
+            </div>
+
+            <span className="text-base text-foreground/60">{role}</span>
+          </div>
+
+          <p className="text-base text-foreground/80 leading-4.5 line-clamp-3">
+            {description}
+          </p>
+          <div className="w-full flex items-center justify-end">
+            <div className="bg-background rounded-sm border border-dark-gray p-1 text-xs text-gray/50 flex items-center justify-center w-fit group-hover:border-gray/20 group-hover:text-gray">
+              {t("seeMore")}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
